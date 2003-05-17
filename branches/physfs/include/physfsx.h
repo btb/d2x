f9b9c68c465601ec76eed218e530728518fab38e
btb <btb@f606b4e5-9f4d-0410-acd1-b66f823a2bfa>
2003-05-17 10:30:06 +0000

    added gets and puts functions

--- branches/physfs/include/physfsx.h
+++ branches/physfs/include/physfsx.h
@@ -1,4 +1,4 @@
-/* $Id: physfsx.h,v 1.1.2.1 2003-05-17 04:42:17 btb Exp $ */
+/* $Id: physfsx.h,v 1.1.2.2 2003-05-17 10:30:06 btb Exp $ */
 
 /*
  *
@@ -27,6 +27,20 @@ static inline int PHYSFSX_readString(PHYSFS_file *file, char *s)
 	return strlen(s);
 }
 
+static inline int PHYSFSX_gets(PHYSFS_file *file, char *s)
+{
+	char *ptr = s;
+
+	if (PHYSFS_eof(file))
+		*ptr = 0;
+	else
+		do
+			PHYSFSX_readU8(file, ptr);
+		while (!PHYSFS_eof(file) && *ptr++!='\n');
+
+	return strlen(s);
+}
+
 static inline int PHYSFSX_writeU8(PHYSFS_file *file, PHYSFS_uint8 val)
 {
 	return PHYSFS_write(file, &val, 1, 1);
@@ -35,6 +49,11 @@ static inline int PHYSFSX_writeU8(PHYSFS_file *file, PHYSFS_uint8 val)
 static inline int PHYSFSX_writeString(PHYSFS_file *file, char *s)
 {
 	return PHYSFS_write(file, s, 1, strlen(s) + 1);
+}
+
+static inline int PHYSFSX_puts(PHYSFS_file *file, char *s)
+{
+	return PHYSFS_write(file, s, 1, strlen(s));
 }
 
 #endif /* PHYSFSX_H */
