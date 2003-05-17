b2c97a6459cdec92d9f8af427883501940e7a09d
btb <btb@f606b4e5-9f4d-0410-acd1-b66f823a2bfa>
2003-05-17 04:36:03 +0000

    made cfile into a wrapper for physfs

--- branches/physfs/include/cfile.h
+++ branches/physfs/include/cfile.h
@@ -1,60 +1,8 @@
-/* $Id: cfile.h,v 1.8 2003-04-12 00:11:46 btb Exp $ */
-/*
-THE COMPUTER CODE CONTAINED HEREIN IS THE SOLE PROPERTY OF PARALLAX
-SOFTWARE CORPORATION ("PARALLAX").  PARALLAX, IN DISTRIBUTING THE CODE TO
-END-USERS, AND SUBJECT TO ALL OF THE TERMS AND CONDITIONS HEREIN, GRANTS A
-ROYALTY-FREE, PERPETUAL LICENSE TO SUCH END-USERS FOR USE BY SUCH END-USERS
-IN USING, DISPLAYING,  AND CREATING DERIVATIVE WORKS THEREOF, SO LONG AS
-SUCH USE, DISPLAY OR CREATION IS FOR NON-COMMERCIAL, ROYALTY OR REVENUE
-FREE PURPOSES.  IN NO EVENT SHALL THE END-USER USE THE COMPUTER CODE
-CONTAINED HEREIN FOR REVENUE-BEARING PURPOSES.  THE END-USER UNDERSTANDS
-AND AGREES TO THE TERMS HEREIN AND ACCEPTS THE SAME BY USE OF THIS FILE.
-COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
-*/
+/* $Id: cfile.h,v 1.8.2.1 2003-05-17 04:35:51 btb Exp $ */
 
 /*
  *
- * Prototypes for compressed file functions...
- *
- * Old Log:
- * Revision 1.1  1995/03/30  10:25:08  allender
- * Initial revision
- *
- *
- * -- PC RCS Information ---
- * Revision 1.10  1995/03/13  15:16:47  john
- * Added alternate directory stuff.
- *
- * Revision 1.9  1995/02/01  20:56:40  john
- * Added cfexist function
- *
- * Revision 1.8  1995/01/21  17:53:41  john
- * Added alternate pig file thing.
- *
- * Revision 1.7  1994/12/12  13:19:47  john
- * Made cfile work with fiellentth.
- *
- * Revision 1.6  1994/12/08  19:02:52  john
- * Added cfgets.
- *
- * Revision 1.5  1994/12/07  21:34:07  john
- * Stripped out compression stuff...
- *
- * Revision 1.4  1994/07/13  00:16:53  matt
- * Added include
- *
- * Revision 1.3  1994/02/17  17:36:19  john
- * Added CF_READ_MODE and CF_WRITE_MODE constants.
- *
- * Revision 1.2  1994/02/15  12:52:08  john
- * Crappy inbetween version
- *
- * Revision 1.1  1994/02/15  10:54:23  john
- * Initial revision
- *
- * Revision 1.1  1994/02/10  15:50:54  john
- * Initial revision
- *
+ * Wrappers for physfs abstraction layer
  *
  */
 
@@ -63,62 +11,194 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 
 #include <stdio.h>
 
+#include <physfs.h>
+
+#define CFILE            PHYSFS_file
+#define cfopen(n,m)      PHYSFS_openRead(n)
+#define cfread(p,s,n,fp) PHYSFS_read(fp,p,s,n)
+#define cfclose          PHYSFS_close
+#define cftell           PHYSFS_tell
+#define cfexist          PHYSFS_exists
+#define cfile_use_alternate_hogfile(n) PHYSFS_addToSearchPath(n,1)
+#define cfile_use_descent1_hogfile(n)  PHYSFS_addToSearchPath(n,1)
+#define cfilelength      PHYSFS_fileLength
+
+//Specify the name of the hogfile.  Returns 1 if hogfile found & had files
+static inline int cfile_init(char *hogname)
+{
+	const char *path = PHYSFS_getRealDir(hogname);
+	char pathname[1024];
+
+	if (!path)
+		return 0;
+
+	snprintf(pathname, 1024, "%s/%s", path, hogname);
+
+	return PHYSFS_addToSearchPath(pathname, 1);
+}
+
+static inline int cfile_size(char *hogname)
+{
+	PHYSFS_file *fp;
+	int size;
+
+	fp = PHYSFS_openRead(hogname);
+	if (fp == NULL)
+		return -1;
+	size = PHYSFS_fileLength(fp);
+	cfclose(fp);
+
+	return size;
+}
+
+static inline int cfgetc(PHYSFS_file *const fp)
+{
+	unsigned char c;
+
+	if (PHYSFS_read(fp, &c, 1, 1) != 1)
+		return EOF;
+
+	return c;
+}
+
+static inline char * cfgets(char * const str, const int size, PHYSFS_file *const fp)
+{
+	int i = 0;
+	int c;
+
+	do
+	{
+		if (i == size-1)
+			break;
+		c = cfgetc(fp);
+		if (c == EOF)
+			break;
+		str[i++] = c;
+	} while (c != '\0' &&
+			 c != -1 &&
+			 c != '\n');
+	str[i] = '\0';
+	if (i == 0)
+		return NULL;
+
+	return str;
+}
+
+static inline int cfseek(PHYSFS_file *fp, long int offset, int where)
+{
+	int c, goal_position;
+
+	switch(where)
+	{
+	case SEEK_SET:
+		goal_position = offset;
+		break;
+	case SEEK_CUR:
+		goal_position = PHYSFS_tell(fp) + offset;
+		break;
+	case SEEK_END:
+		goal_position = PHYSFS_fileLength(fp) + offset;
+		break;
+	default:
+		return 1;
+	}
+	c = PHYSFS_seek(fp, goal_position);
+	return c;
+}
+
+
+/*
+ * read some data types...
+ */
+
+#include "pstypes.h"
 #include "maths.h"
 #include "vecmat.h"
 
-typedef struct CFILE {
-	FILE 			*file;
-	int				size;
-	int				lib_offset;
-	int				raw_position;
-} CFILE;
+static inline int cfile_read_int(PHYSFS_file *file)
+{
+	int32_t i;
 
-//Specify the name of the hogfile.  Returns 1 if hogfile found & had files
-int cfile_init(char *hogname);
+	if (!PHYSFS_readULE32(file, &i))
+	{
+		fprintf(stderr, "Error reading int in cfile_read_int()");
+		exit(1);
+	}
 
-int cfile_size(char *hogname);
+	return i;
+}
 
-CFILE * cfopen(char * filename, char * mode);
-int cfilelength( CFILE *fp );							// Returns actual size of file...
-size_t cfread( void * buf, size_t elsize, size_t nelem, CFILE * fp );
-void cfclose( CFILE * cfile );
-int cfgetc( CFILE * fp );
-int cfseek( CFILE *fp, long int offset, int where );
-int cftell( CFILE * fp );
-char * cfgets( char * buf, size_t n, CFILE * fp );
+static inline short cfile_read_short(PHYSFS_file *file)
+{
+	int16_t s;
 
-int cfexist( char * filename );	// Returns true if file exists on disk (1) or in hog (2).
+	if (!PHYSFS_readULE16(file, &s))
+	{
+		fprintf(stderr, "Error reading short in cfile_read_short()");
+		exit(1);
+	}
 
-// Allows files to be gotten from an alternate hog file.
-// Passing NULL disables this.
-// Returns 1 if hogfile found (& contains file), else 0.  
-// If NULL passed, returns 1
-int cfile_use_alternate_hogfile( char * name );
+	return s;
+}
 
-// Allows files to be gotten from the Descent 1 hog file.
-// Passing NULL disables this.
-// Returns 1 if hogfile found (& contains file), else 0.
-// If NULL passed, returns 1
-int cfile_use_descent1_hogfile(char * name);
+static inline byte cfile_read_byte(PHYSFS_file *file)
+{
+	byte b;
 
-// All cfile functions will check this directory if no file exists
-// in the current directory.
-void cfile_use_alternate_hogdir( char * path );
+	if (PHYSFS_read(file, &b, sizeof(b), 1) != 1)
+	{
+		fprintf(stderr, "Error reading byte in cfile_read_byte()");
+		exit(1);
+	}
 
-//tell cfile about your critical error counter 
-void cfile_set_critical_error_counter_ptr(int *ptr);
+	return b;
+}
 
-// prototypes for reading basic types from cfile
-int cfile_read_int(CFILE *file);
-short cfile_read_short(CFILE *file);
-byte cfile_read_byte(CFILE *file);
-fix cfile_read_fix(CFILE *file);
-fixang cfile_read_fixang(CFILE *file);
-void cfile_read_vector(vms_vector *v, CFILE *file);
-void cfile_read_angvec(vms_angvec *v, CFILE *file);
-void cfile_read_matrix(vms_matrix *v, CFILE *file);
+static inline fix cfile_read_fix(PHYSFS_file *file)
+{
+	fix f;
 
-extern char AltHogDir[];
-extern char AltHogdir_initialized;
+	if (!PHYSFS_readULE32(file, &f))
+	{
+		fprintf(stderr, "Error reading fix in cfile_read_fix()");
+		exit(1);
+	}
+
+	return f;
+}
+
+static inline fixang cfile_read_fixang(PHYSFS_file *file)
+{
+	fixang f;
+
+	if (!PHYSFS_readULE16(file, &f))
+	{
+		fprintf(stderr, "Error reading fixang in cfile_read_fixang()");
+		exit(1);
+	}
+
+	return f;
+}
+
+static inline void cfile_read_vector(vms_vector *v, PHYSFS_file *file)
+{
+	v->x = cfile_read_fix(file);
+	v->y = cfile_read_fix(file);
+	v->z = cfile_read_fix(file);
+}
+
+static inline void cfile_read_angvec(vms_angvec *v, PHYSFS_file *file)
+{
+	v->p = cfile_read_fixang(file);
+	v->b = cfile_read_fixang(file);
+	v->h = cfile_read_fixang(file);
+}
+
+static inline void cfile_read_matrix(vms_matrix *m,PHYSFS_file *file)
+{
+	cfile_read_vector(&m->rvec,file);
+	cfile_read_vector(&m->uvec,file);
+	cfile_read_vector(&m->fvec,file);
+}
 
 #endif
