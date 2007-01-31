99953c853e38fa12dd2136d6a6d75eb4bb78ce24
btb <btb@f606b4e5-9f4d-0410-acd1-b66f823a2bfa>
2007-01-31 04:20:05 +0000

    amd64 fixes

--- trunk/iff/iff.c
+++ trunk/iff/iff.c
@@ -1,4 +1,4 @@
-/* $Id: iff.c,v 1.10 2004-12-01 06:57:28 btb Exp $ */
+/* $Id: iff.c,v 1.11 2007-01-31 04:20:05 btb Exp $ */
 /*
 THE COMPUTER CODE CONTAINED HEREIN IS THE SOLE PROPERTY OF PARALLAX
 SOFTWARE CORPORATION ("PARALLAX").  PARALLAX, IN DISTRIBUTING THE CODE TO
@@ -23,7 +23,7 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #endif
 
 #ifdef RCS
-static char rcsid[] = "$Id: iff.c,v 1.10 2004-12-01 06:57:28 btb Exp $";
+static char rcsid[] = "$Id: iff.c,v 1.11 2007-01-31 04:20:05 btb Exp $";
 #endif
 
 #define COMPRESS		1	//do the RLE or not? (for debugging mostly)
@@ -86,7 +86,7 @@ typedef struct fake_file {
 	int length;
 } FFILE;
 
-#define MAKE_SIG(a,b,c,d) (((long)(a)<<24)+((long)(b)<<16)+((c)<<8)+(d))
+#define MAKE_SIG(a,b,c,d) (((int32_t)(a)<<24)+((int32_t)(b)<<16)+((c)<<8)+(d))
 
 #define form_sig MAKE_SIG('F','O','R','M')
 #define ilbm_sig MAKE_SIG('I','L','B','M')
@@ -100,7 +100,7 @@ typedef struct fake_file {
 #define dlta_sig MAKE_SIG('D','L','T','A')
 
 #ifndef NDEBUG
-//void printsig(long s)
+//void printsig(int32_t s)
 //{
 //	char *t=(char *) &s;
 //
@@ -109,7 +109,7 @@ typedef struct fake_file {
 //}
 #endif
 
-long get_sig(FFILE *f)
+int32_t get_sig(FFILE *f)
 {
 	char s[4];
 
@@ -138,10 +138,10 @@ long get_sig(FFILE *f)
 	s[3] = f->data[f->position++];
 #endif
 
-	return(*((long *) s));
+	return(*((int32_t *) s));
 }
 
-int put_sig(long sig,FILE *f)
+int put_sig(int32_t sig,FILE *f)
 {
 	char *s = (char *) &sig;
 
