d0e9efb9aa057a001fd55557b28f130f99cc535f
Bradley Bell <btb@icculus.org>
2014-11-26 23:14:10 -0800

    use correct size for sigs

--- include/makesig.h
+++ include/makesig.h
@@ -13,6 +13,6 @@
 #ifndef _MAKESIG_H
 #define _MAKESIG_H
 
-#define MAKE_SIG(a,b,c,d) (((long)(a)<<24)+((long)(b)<<16)+((c)<<8)+(d))
+#define MAKE_SIG(a,b,c,d) (((uint32_t)(a)<<24)+((uint32_t)(b)<<16)+((c)<<8)+(d))
 
 #endif
