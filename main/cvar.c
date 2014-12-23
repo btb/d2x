31b48db0257d07eb3bd65341c6bfede0d97a7680
Bradley Bell <btb@icculus.org>
2014-12-22 17:12:29 -0800

    keep cvar string value around

--- main/cvar.c
+++ main/cvar.c
@@ -10,21 +10,46 @@
 #include "cvar.h"
 #include "error.h"
 #include "strutil.h"
+#include "u_mem.h"
 
 
 /* The list of cvars */
 cvar_t *cvar_list = NULL;
+
+int cvar_initialized = 0;
+
+
+void cvar_free(void)
+{
+	cvar_t *ptr;
+
+	for (ptr = cvar_list; ptr != NULL; ptr = ptr->next)
+		d_free(ptr->string);
+}
+
+
+void cvar_init(void)
+{
+	atexit(cvar_free);
+	cvar_initialized = 1;
+}
 
 
 /* Register a cvar */
 void cvar_registervariable (cvar_t *cvar)
 {
 	cvar_t *ptr;
+	char *stringval;
+
+	if (!cvar_initialized)
+		cvar_init();
 
 	Assert(cvar != NULL);
 
+	stringval = cvar->string;
 	cvar->next = NULL;
 	cvar->value = strtod(cvar->string, (char **) NULL);
+	cvar->string = d_strdup(stringval);
 
 	if (cvar_list == NULL)
 	{
@@ -47,6 +72,8 @@ void cvar_set (char *cvar_name, char *value)
 
 	if (ptr == NULL) return; // If we didn't find the cvar, give up
 
+	d_free(ptr->string);
+	ptr->string = d_strdup(value);
 	ptr->value = strtod(value, (char **) NULL);
 }
 
