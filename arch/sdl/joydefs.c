0429f1922532c554798cdbaf93fda02be5faac08
Bradley Bell <btb@icculus.org>
2015-01-26 18:46:31 -0800

    add joystick deadzone, mouse sensitivity config

--- arch/sdl/joydefs.c
+++ arch/sdl/joydefs.c
@@ -49,35 +49,70 @@ void joydef_menuset_1(int nitems, newmenu_item * items, int *last_key, int citem
 }
 
 
+#define ADD_SLIDER(txt,v,minv,maxv) do { m[nitems].type=NM_TYPE_SLIDER; m[nitems].text=txt; m[nitems].value=v; m[nitems].min_value=minv; m[nitems].max_value=maxv; nitems++; } while (0)
+
 void joydefs_sensitivity(void)
 {
-	newmenu_item m[6];
+	newmenu_item m[23];
 	int i1 = 0;
-	int nitems = 6;
+	int nitems = 0;
 
-	m[0].type = NM_TYPE_SLIDER; m[0].text = TXT_TURN_LR;  m[0].value = f2i(8 * Config_joystick_sensitivity[AXIS_TURN-1].value);      m[0].min_value = 0; m[0].max_value = 16;
-	m[1].type = NM_TYPE_SLIDER; m[1].text = TXT_PITCH_UD; m[1].value = f2i(8 * Config_joystick_sensitivity[AXIS_PITCH-1].value);     m[1].min_value = 0; m[1].max_value = 16;
-	m[2].type = NM_TYPE_SLIDER; m[2].text = TXT_SLIDE_LR; m[2].value = f2i(8 * Config_joystick_sensitivity[AXIS_LEFTRIGHT-1].value); m[2].min_value = 0; m[2].max_value = 16;
-	m[3].type = NM_TYPE_SLIDER; m[3].text = TXT_SLIDE_UD; m[3].value = f2i(8 * Config_joystick_sensitivity[AXIS_UPDOWN-1].value);    m[3].min_value = 0; m[3].max_value = 16;
-	m[4].type = NM_TYPE_SLIDER; m[4].text = TXT_BANK_LR;  m[4].value = f2i(8 * Config_joystick_sensitivity[AXIS_BANK-1].value);      m[4].min_value = 0; m[4].max_value = 16;
-	m[5].type = NM_TYPE_SLIDER; m[5].text = TXT_THROTTLE; m[5].value = f2i(8 * Config_joystick_sensitivity[AXIS_THROTTLE-1].value);  m[5].min_value = 0; m[5].max_value = 16;
+	m[nitems].type = NM_TYPE_TEXT; m[nitems].text = "Joystick Sensitivity"; nitems++;
+	ADD_SLIDER(TXT_TURN_LR,  f2i(8 * Config_joystick_sensitivity[AXIS_TURN-1].value), 0, 16);
+	ADD_SLIDER(TXT_PITCH_UD, f2i(8 * Config_joystick_sensitivity[AXIS_PITCH-1].value), 0, 16);
+	ADD_SLIDER(TXT_SLIDE_LR, f2i(8 * Config_joystick_sensitivity[AXIS_LEFTRIGHT-1].value), 0, 16);
+	ADD_SLIDER(TXT_SLIDE_UD, f2i(8 * Config_joystick_sensitivity[AXIS_UPDOWN-1].value), 0, 16);
+	ADD_SLIDER(TXT_BANK_LR,  f2i(8 * Config_joystick_sensitivity[AXIS_BANK-1].value), 0, 16);
+	ADD_SLIDER(TXT_THROTTLE, f2i(8 * Config_joystick_sensitivity[AXIS_THROTTLE-1].value), 0, 16);
+
+	m[nitems].type = NM_TYPE_TEXT; m[nitems].text = ""; nitems++;
+	m[nitems].type = NM_TYPE_TEXT; m[nitems].text = "Joystick Deadzone"; nitems++;
+	ADD_SLIDER(TXT_TURN_LR,  f2i(8 * Config_joystick_deadzone[AXIS_TURN-1].value), 0, 16);
+	ADD_SLIDER(TXT_PITCH_UD, f2i(8 * Config_joystick_deadzone[AXIS_PITCH-1].value), 0, 16);
+	ADD_SLIDER(TXT_SLIDE_LR, f2i(8 * Config_joystick_deadzone[AXIS_LEFTRIGHT-1].value), 0, 16);
+	ADD_SLIDER(TXT_SLIDE_UD, f2i(8 * Config_joystick_deadzone[AXIS_UPDOWN-1].value), 0, 16);
+	ADD_SLIDER(TXT_BANK_LR,  f2i(8 * Config_joystick_deadzone[AXIS_BANK-1].value), 0, 16);
+	ADD_SLIDER(TXT_THROTTLE, f2i(8 * Config_joystick_deadzone[AXIS_THROTTLE-1].value), 0, 16);
+
+	m[nitems].type = NM_TYPE_TEXT; m[nitems].text = ""; nitems++;
+	m[nitems].type = NM_TYPE_TEXT; m[nitems].text = "Mouse Sensitivity"; nitems++;
+	ADD_SLIDER(TXT_TURN_LR,  f2i(8 * Config_mouse_sensitivity[AXIS_TURN-1].value), 0, 16);
+	ADD_SLIDER(TXT_PITCH_UD, f2i(8 * Config_mouse_sensitivity[AXIS_PITCH-1].value), 0, 16);
+	ADD_SLIDER(TXT_SLIDE_LR, f2i(8 * Config_mouse_sensitivity[AXIS_LEFTRIGHT-1].value), 0, 16);
+	ADD_SLIDER(TXT_SLIDE_UD, f2i(8 * Config_mouse_sensitivity[AXIS_UPDOWN-1].value), 0, 16);
+	ADD_SLIDER(TXT_BANK_LR,  f2i(8 * Config_mouse_sensitivity[AXIS_BANK-1].value), 0, 16);
+	ADD_SLIDER(TXT_THROTTLE, f2i(8 * Config_mouse_sensitivity[AXIS_THROTTLE-1].value), 0, 16);
 
 	do
-		i1 = newmenu_do1(NULL, TXT_JOYS_SENSITIVITY, nitems, m, NULL, i1);
+		i1 = newmenu_do1(NULL, "Sensitivity", nitems, m, NULL, i1);
 	while ( i1 > -1 );
 
-	cvar_setfl(&Config_joystick_sensitivity[AXIS_TURN-1],      m[0].value / 8.0);
-	cvar_setfl(&Config_joystick_sensitivity[AXIS_PITCH-1],     m[1].value / 8.0);
-	cvar_setfl(&Config_joystick_sensitivity[AXIS_LEFTRIGHT-1], m[2].value / 8.0);
-	cvar_setfl(&Config_joystick_sensitivity[AXIS_UPDOWN-1],    m[3].value / 8.0);
-	cvar_setfl(&Config_joystick_sensitivity[AXIS_BANK-1],      m[4].value / 8.0);
-	cvar_setfl(&Config_joystick_sensitivity[AXIS_THROTTLE-1],  m[5].value / 8.0);
+	cvar_setfl(&Config_joystick_sensitivity[AXIS_TURN-1],      m[1].value / 8.0);
+	cvar_setfl(&Config_joystick_sensitivity[AXIS_PITCH-1],     m[2].value / 8.0);
+	cvar_setfl(&Config_joystick_sensitivity[AXIS_LEFTRIGHT-1], m[3].value / 8.0);
+	cvar_setfl(&Config_joystick_sensitivity[AXIS_UPDOWN-1],    m[4].value / 8.0);
+	cvar_setfl(&Config_joystick_sensitivity[AXIS_BANK-1],      m[5].value / 8.0);
+	cvar_setfl(&Config_joystick_sensitivity[AXIS_THROTTLE-1],  m[6].value / 8.0);
+
+	cvar_setfl(&Config_joystick_deadzone[AXIS_TURN-1],      m[ 9].value / 16.0);
+	cvar_setfl(&Config_joystick_deadzone[AXIS_PITCH-1],     m[10].value / 16.0);
+	cvar_setfl(&Config_joystick_deadzone[AXIS_LEFTRIGHT-1], m[11].value / 16.0);
+	cvar_setfl(&Config_joystick_deadzone[AXIS_UPDOWN-1],    m[12].value / 16.0);
+	cvar_setfl(&Config_joystick_deadzone[AXIS_BANK-1],      m[13].value / 16.0);
+	cvar_setfl(&Config_joystick_deadzone[AXIS_THROTTLE-1],  m[14].value / 16.0);
+
+	cvar_setfl(&Config_mouse_sensitivity[AXIS_TURN-1],      m[17].value / 8.0);
+	cvar_setfl(&Config_mouse_sensitivity[AXIS_PITCH-1],     m[18].value / 8.0);
+	cvar_setfl(&Config_mouse_sensitivity[AXIS_LEFTRIGHT-1], m[19].value / 8.0);
+	cvar_setfl(&Config_mouse_sensitivity[AXIS_UPDOWN-1],    m[20].value / 8.0);
+	cvar_setfl(&Config_mouse_sensitivity[AXIS_BANK-1],      m[21].value / 8.0);
+	cvar_setfl(&Config_mouse_sensitivity[AXIS_THROTTLE-1],  m[22].value / 8.0);
 }
 
 
 void joydefs_config()
 {
-	newmenu_item m[13];
+	newmenu_item m[9];
 	int i1 = 3;
 	int nitems = 9;
 
