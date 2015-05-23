a95e45ea9d4947af2c5ff04814ca430812d9182d
Bradley Bell <btb@icculus.org>
2015-05-22 18:37:36 -0700

    fix up autotools stuff

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
-echo "If you get errors, you may need newer versions of automake and autoconf."
-echo "You'll need at least automake 1.5 and autoconf 2.50."
-aclocal $ACLOCAL_FLAGS
+aclocal -Im4
 autoheader
 automake --add-missing --copy
 autoconf
