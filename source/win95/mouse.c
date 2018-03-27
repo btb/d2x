37abdd5f79edec339842749055e07ae8db2fbeef
Bradley Bell <btb@icculus.org>
2018-03-26 20:46:01 -0700

    don't use dos conio on windows

--- source/win95/mouse.c
+++ source/win95/mouse.c
@@ -18,7 +18,6 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include <windows.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <conio.h>
 #include <string.h>
 #include <i86.h>
 
