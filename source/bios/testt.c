495e43f72b822cf4019763d439815fafa1f1d0d1
Bradley Bell <btb@icculus.org>
2018-03-28 22:23:25 -0700

    get the timer test working, using fixed seconds

--- source/bios/testt.c
+++ source/bios/testt.c
@@ -14,42 +14,45 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #include <stdio.h>
 
 #include "timer.h"
-#include "key.h"
+// #include "key.h"
+#include "fix.h"
 
+#define TICKER (*(volatile unsigned int *)0x46C)
 
 void main (void)
 {
-	unsigned int MyTimer, t,t1, t2, ot, myc;
+	fix MyTimer, t, t1, t2, ot, myc;
 	int i,s, b;
 
-	key_init();
+	// key_init();
 
 	//timer_init( 4661, NULL );
 	timer_init( 0, NULL );
 
 	s = TICKER;
-	MyTimer = timer_get_microseconds();
+	MyTimer = timer_get_fixed_seconds();
 
-	while(!keyd_pressed[KEY_ESC])
+	t1 = 0;
+	while(t1 < 655360)
 	{
 		delay(1);
 		ot =t;
-		t = timer_get_milliseconds();
-		t1 = (TICKER - s)*10000/182;
+		t = timer_get_fixed_seconds();
+		t1 = (TICKER - s)*655360/182;
 		//printf( "%d\t%d\t%u\t%d\t%u\t%u\t%u\n", (TICKER-s)*10000/182, myc, t, (int)t-(int)ot, key_down_time(KEY_G), key_down_count(KEY_G), key_up_count(KEY_G) );
 		printf( "%u\t%u\t%d\n", t1, t, (int)t - (int)t1 );
 	}
 
-	t1 = timer_get_microseconds();
+	t1 = timer_get_fixed_seconds();
 	delay(100);
-	t2 = timer_get_microseconds();
+	t2 = timer_get_fixed_seconds();
 
-	printf( "This number should be about 100000:  %d\n", t2-t1 );
+	printf( "This number should be about %d:  %d\n", F0_1, t2-t1 );
 
-	t1 = timer_get_microseconds();
-	t2 = timer_get_microseconds();
+	t1 = timer_get_fixed_seconds();
+	t2 = timer_get_fixed_seconds();
 
-	printf( "Overhead for 'timer_get_microseconds' call:  %d \xE6Seconds\n", t2-t1 );
+	printf( "Overhead for 'timer_get_fixed_seconds' call:  %f Seconds\n", f2fl(t2-t1) );
 
 
 	timer_close();
