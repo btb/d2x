635f94cccd3efef4eb1a9c86b06d60851c38b656
Bradley Bell <btb@icculus.org>
2018-04-16 03:33:16 -0700

    Move palette.c back to gr library
    factor out duplicate code

--- source/win95/palette.c
+++ source/win95/palette.c
@@ -42,20 +42,11 @@ typedef struct LOGPAL256 {
 
 
 //	Special --------------------------------------------------------------------
-extern int gr_installed;
+extern ubyte gr_current_pal[256*3];        // Current Valid Palette in RGB
+extern ubyte gr_palette_gamma;
 
+void init_computed_colors(void);
 
-//	Globals --------------------------------------------------------------------	
-
-ubyte gr_palette[256*3];					// Main Palette in RGB
-ubyte gr_current_pal[256*3];				// Current Valid Palette in RGB
-ubyte gr_fade_table[256*34];			 	// Fade Palette Table in RGB
-
-ubyte	gr_palette_gamma = 0;
-int 	gr_palette_gamma_param = 0;
-ubyte	gr_palette_faded_out = 1;
-
-int grd_fades_disabled=0;					// Used to skip fading for development
 
 static LPDIRECTDRAWPALETTE	_lpDDPalActive = 0; 	
 static LPDIRECTDRAW	lpDD = NULL;
@@ -125,174 +116,6 @@ void grwin_gdi_realizepal(HDC hdc)
 
 
 //	----------------------------------------------------------------------------
-
-void gr_palette_set_gamma( int gamma )
-{
-	if ( gamma < 0 ) gamma = 0;
-	if ( gamma > 8 ) gamma = 8;
-
-	if (gr_palette_gamma_param != gamma )	{
-		gr_palette_gamma_param = gamma;
-		gr_palette_gamma = gamma;
-		if (!gr_palette_faded_out)	{
-			gr_palette_load( gr_palette );
-		}
-	}	
-}
-
-int gr_palette_get_gamma()
-{
-	return gr_palette_gamma_param;
-}
-
-
-void gr_use_palette_table( char * filename )
-{
-	CFILE *fp;
-	int i,fsize;
-
-	fp = cfopen( filename, "rb" );
-	if ( fp==NULL)
-		Error("Can't open palette file <%s>",filename);
-
-	fsize	= cfilelength( fp );
-	Assert( fsize == 9472 );
-	cfread( gr_palette, 256*3, 1, fp );
-	cfread( gr_fade_table, 256*34, 1, fp );
-	cfclose(fp);
-
-	// This is the TRANSPARENCY COLOR
-	for (i=0; i<GR_FADE_LEVELS; i++ )	{
-		gr_fade_table[i*256+255] = 255;
-	}
-
-}
-
-#define SQUARE(x) ((x)*(x))
-
-#define	MAX_COMPUTED_COLORS	32
-
-int	Num_computed_colors=0;
-
-typedef struct {
-	ubyte	r,g,b,color_num;
-} color_record;
-
-color_record Computed_colors[MAX_COMPUTED_COLORS];
-
-//	Add a computed color (by gr_find_closest_color) to list of computed colors in Computed_colors.
-//	If list wasn't full already, increment Num_computed_colors.
-//	If was full, replace a random one.
-void add_computed_color(int r, int g, int b, int color_num)
-{
-	int	add_index;
-
-	if (Num_computed_colors < MAX_COMPUTED_COLORS) {
-		add_index = Num_computed_colors;
-		Num_computed_colors++;
-	} else
-		add_index = (rand() * MAX_COMPUTED_COLORS) >> 15;
-
-	Computed_colors[add_index].r = r;
-	Computed_colors[add_index].g = g;
-	Computed_colors[add_index].b = b;
-	Computed_colors[add_index].color_num = color_num;
-}
-
-void init_computed_colors(void)
-{
-	int	i;
-
-	for (i=0; i<MAX_COMPUTED_COLORS; i++)
-		Computed_colors[i].r = 255;		//	Make impossible to match.
-}
-
-int gr_find_closest_color( int r, int g, int b )
-{
-	int i, j;
-	int best_value, best_index, value;
-
-	if (Num_computed_colors == 0)
-		init_computed_colors();
-
-	//	If we've already computed this color, return it!
-	for (i=0; i<Num_computed_colors; i++)
-		if (r == Computed_colors[i].r)
-			if (g == Computed_colors[i].g)
-				if (b == Computed_colors[i].b) {
-					if (i > 4) {
-						color_record	trec;
-						trec = Computed_colors[i-1];
-						Computed_colors[i-1] = Computed_colors[i];
-						Computed_colors[i] = trec;
-						return Computed_colors[i-1].color_num;
-					}
-					return Computed_colors[i].color_num;
-				}
-
-//	r &= 63;
-//	g &= 63;
-//	b &= 63;
-
-	best_value = SQUARE(r-gr_palette[0])+SQUARE(g-gr_palette[1])+SQUARE(b-gr_palette[2]);
-	best_index = 0;
-	if (best_value==0) {
-		add_computed_color(r, g, b, best_index);
- 		return best_index;
-	}
-	j=0;
-	// only go to 255, 'cause we dont want to check the transparent color.
-	for (i=1; i<254; i++ )	{
-		j += 3;
-		value = SQUARE(r-gr_palette[j])+SQUARE(g-gr_palette[j+1])+SQUARE(b-gr_palette[j+2]);
-		if ( value < best_value )	{
-			if (value==0) {
-				add_computed_color(r, g, b, i);
-				return i;
-			}
-			best_value = value;
-			best_index = i;
-		}
-	}
-	add_computed_color(r, g, b, best_index);
-	return best_index;
-}
-
-int gr_find_closest_color_15bpp( int rgb )
-{
-	return gr_find_closest_color( ((rgb>>10)&31)*2, ((rgb>>5)&31)*2, (rgb&31)*2 );
-}
-
-
-int gr_find_closest_color_current( int r, int g, int b )
-{
-	int i, j;
-	int best_value, best_index, value;
-
-//	r &= 63;
-//	g &= 63;
-//	b &= 63;
-
-	best_value = SQUARE(r-gr_current_pal[0])+SQUARE(g-gr_current_pal[1])+SQUARE(b-gr_current_pal[2]);
-	best_index = 0;
-	if (best_value==0)
- 		return best_index;
-
-	j=0;
-	// only go to 255, 'cause we dont want to check the transparent color.
-	for (i=1; i<254; i++ )	{
-		j += 3;
-		value = SQUARE(r-gr_current_pal[j])+SQUARE(g-gr_current_pal[j+1])+SQUARE(b-gr_current_pal[j+2]);
-		if ( value < best_value )	{
-			if (value==0)
-				return i;
-			best_value = value;
-			best_index = i;
-		}
-	}
-	return best_index;
-}
-
 
 static int last_r=0, last_g=0, last_b=0;
 
@@ -569,22 +392,6 @@ int gr_palette_fade_in(ubyte *pal, int nsteps, int allow_keys)
 	return 0;
 }
 
-void gr_make_cthru_table(ubyte * table, ubyte r, ubyte g, ubyte b )
-{
-	int i;
-	ubyte r1, g1, b1;
-
-	for (i=0; i<256; i++ )	{
-		r1 = gr_palette[i*3+0] + r;
-		if ( r1 > 63 ) r1 = 63;
-		g1 = gr_palette[i*3+1] + g;
-		if ( g1 > 63 ) g1 = 63;
-		b1 = gr_palette[i*3+2] + b;
-		if ( b1 > 63 ) b1 = 63;
-		table[i] = gr_find_closest_color( r1, g1, b1 );
-	}
-}
-
 void gr_palette_read(ubyte * palette)
 {
 	int i;
@@ -605,15 +412,6 @@ void gr_palette_read(ubyte * palette)
 		*palette++ = PalGDIData.ScratchPal[i].peGreen >> 2;
 		*palette++ = PalGDIData.ScratchPal[i].peBlue >> 2;
 	}
-}
-
-
-
-void gr_copy_palette(ubyte *gr_palette, ubyte *pal, int size)
-{
-	memcpy(gr_palette, pal, size);
-
-	Num_computed_colors = 0;
 }
 
 
