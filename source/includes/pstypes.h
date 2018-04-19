e4229151d91e02cf0e2845839ca5d04d042ff117
Bradley Bell <btb@icculus.org>
2018-04-19 15:32:39 -0700

    int types for old MSVC versions

--- source/includes/pstypes.h
+++ source/includes/pstypes.h
@@ -14,6 +14,20 @@ COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
 #ifndef _TYPES_H
 #define _TYPES_H
 
+#if defined(_MSC_VER) && _MSC_VER < 1700
+typedef __int8              int8_t;
+typedef unsigned __int8     uint8_t;
+typedef __int16             int16_t;
+typedef unsigned __int16    uint16_t;
+typedef __int32             int32_t;
+typedef unsigned __int32    uint32_t;
+typedef __int64             int64_t;
+typedef unsigned __int64    uint64_t;
+#else
+#include <inttypes.h>
+#endif
+
+
 //define a byte
 typedef signed char sbyte;
 
