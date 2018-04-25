38f262358f00fddcc4b907d5683275e385a84d79
Bradley Bell <btb@icculus.org>
2018-04-25 02:35:32 -0700

    add sdl palette stubs

---
+++ source/vid/sdl/palette.c
@@ -0,0 +1,34 @@
+
+#include <SDL.h>
+
+#include "pstypes.h"
+#include "palette.h"
+
+
+// Functions ------------------------------------------------------------------
+
+void gr_palette_step_up(int r, int g, int b)
+{
+}
+
+void gr_palette_clear(void)
+{
+}
+
+void gr_palette_load(ubyte *pal)
+{
+}
+
+int gr_palette_fade_out(ubyte *pal, int nsteps, int allow_keys)
+{
+   return 0;
+}
+
+int gr_palette_fade_in(ubyte *pal, int nsteps, int allow_keys)
+{
+   return 0;
+}
+
+void gr_palette_read(ubyte *palette)
+{
+}
