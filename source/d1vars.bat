f6549ef4e382bc607386bf04a68207d18ff34860
Bradley Bell <bradley.bell@gmail.com>
2021-07-20 20:40:52 -0700

    move to source/

--- source/d1vars.bat
+++ source/d1vars.bat
@@ -1,7 +1,7 @@
 @echo off
 set TOOLROOTDIR=c:\MSVC
 set PATH=c:\masm\bin;c:\watcom\binnt;c:\MSVC\BIN;%PATH%
-set INCLUDE=c:\watcom\h;c:\MSVC\INCLUDE;%INCLUDE%;y:\dev\d1\lib;y:\dev\d1\includes;
+set INCLUDE=c:\watcom\h;c:\MSVC\INCLUDE;%INCLUDE%;y:\dev\d1\source\lib;y:\dev\d1\source\includes;
 set LIB=c:\MSVC\LIB;%LIB%
 set watcom=c:\watcom\
 
